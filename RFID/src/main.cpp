4a9de9f236f4605bd8fc66750990210a81c7ff56
Tiago Barros <tsb4@cin.ufpe.br>
2020-09-02 15:27:49 -0300

    Trying to integrate all. Not tested

--- RFID/src/main.cpp
+++ RFID/src/main.cpp
@@ -1,14 +1,27 @@
 #include <SPI.h>
+#include <Wire.h>
 #include <MFRC522.h>
 #include <Arduino.h>
-#include <ESP8266WiFi.h> 
+#include <WiFi.h> 
+#include "MLX90641_API.h"
+#include "MLX9064X_I2C_Driver.h"
+
+#define debug  Serial
+
+const byte MLX90641_address = 0x33; //Default 7-bit unshifted address of the MLX90641
+#define TA_SHIFT 8 //Default shift for MLX90641 in open air
+uint16_t eeMLX90641[832];
+float MLX90641To[192];
+uint16_t MLX90641Frame[242];
+paramsMLX90641 MLX90641;
+int errorno = 0;
 
 const char* ssid = "Heim";
 const char* password = "acgabensb";
 WiFiServer server(80);
 
-#define RST_PIN    D3    
-#define SS_PIN     D4   
+#define RST_PIN    15   
+#define SS_PIN     2   
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
@@ -23,12 +36,16 @@ void printRequestRFID();
 void setupWIFI();
 boolean messageWIFI();
 void generatePacket();
+boolean isConnected();
+void setupMLX();
+void takeImage();
 
 void setup() 
 {
-  Serial.begin(9600);   // Inicia a serial
+  Serial.begin(115200);   // Inicia a serial
   setupRFID();
   setupWIFI();
+  setupMLX();
 }
 
 void loop() 
@@ -44,7 +61,11 @@ void loop()
   }
   if(estado == 2){
     Serial.println("Esperando envio de dados...");
-    for(int i=0;i<192;i++) temp[i] =28.42;
+    delay(1000);
+    takeImage();
+    for(int i=0;i<192;i++){
+      temp[i] = MLX90641To[i];
+    }
     estado =3;
     generatePacket();
   }
@@ -167,4 +188,66 @@ void generatePacket(){
     packet.concat(",");
   }
   Serial.println(packet);
+}
+
+void setupMLX(){
+  Wire.begin();
+  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+ 
+  while (!debug);
+
+  if (isConnected() == false) {
+    debug.println("MLX90641 not detected at default I2C address. Please check wiring. Freezing.");
+    while (1);
+  }
+   //Get device parameters - We only have to do this once
+  int status;
+  status = MLX90641_DumpEE(MLX90641_address, eeMLX90641);
+  errorno = status;//MLX90641_CheckEEPROMValid(eeMLX90641);//eeMLX90641[10] & 0x0040;//
+
+  if (status != 0) {
+    debug.println("Failed to load system parameters");
+    while(1);
+  }
+
+  status = MLX90641_ExtractParameters(eeMLX90641, &MLX90641);
+  //errorno = status;
+  if (status != 0) {
+    debug.println("Parameter extraction failed");
+    while(1);
+  }
+
+  //Once params are extracted, we can release eeMLX90641 array
+
+  //MLX90641_SetRefreshRate(MLX90641_address, 0x02); //Set rate to 2Hz
+  MLX90641_SetRefreshRate(MLX90641_address, 0x03); //Set rate to 4Hz
+  //MLX90641_SetRefreshRate(MLX90641_address, 0x07); //Set rate to 64Hz    
+}
+
+void takeImage(){
+  long startTime = millis();
+ 
+  for (byte x = 0 ; x < 2 ; x++) {
+    int status = MLX90641_GetFrameData(MLX90641_address, MLX90641Frame);
+    float vdd = MLX90641_GetVdd(MLX90641Frame, &MLX90641);
+    float Ta = MLX90641_GetTa(MLX90641Frame, &MLX90641);
+    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float emissivity = 0.95;
+ 
+    MLX90641_CalculateTo(MLX90641Frame, &MLX90641, emissivity, tr, MLX90641To);
+  }
+  long stopTime = millis();
+  for (int x = 0 ; x < 192 ; x++) {
+    debug.print(MLX90641To[x], 2);
+    debug.print(",");
+  }
+  debug.println("");    
+}
+boolean isConnected() {
+ 
+  Wire.beginTransmission((uint8_t)MLX90641_address);
+  if (Wire.endTransmission() != 0) {
+    return (false);    //Sensor did not ACK
+  }
+  return (true);
 }
